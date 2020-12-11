f75289517f5359cc34c59c322fa4abaaef04bcc7
swmock52 <71279758+swmock52@users.noreply.github.com>
2020-12-11 13:01:42 -0500

    Add files via upload

--- Creative Coding/sketch.js
+++ Creative Coding/sketch.js
@@ -1,37 +1,46 @@
-//random
+//3 Random
 
 function setup() {
-  createCanvas(720, 720);
-  noFill ();
-  strokeWeight (3);
-  stroke (255,50,100);//RGB red would be (255,0,0)
-  //frameRate(3); to change speed w/o noLoop();
+    createCanvas(windowWidth, windowWidth);
+    noFill();
+    noStroke();
+    frameRate(1);
+    cursor('https://eeriley99.github.io/code_sketchbook/icons-master/clickFinger.gif');
 }
 
 function draw() {
-  
-  background(220,220,220);
-  
-  var windowPane = 30; // padding around the box array
-  var num = 10; // number of boxes
-  var shift = random (-10, 10); // degree of shift in our quad
-  var space = 0; //space between squares
-  
-  translate(windowPane + space/2,windowPane + space/2); // starting point of window (x,y)
-  
-  var sideLen = (720 - 2 * windowPane)/num; // pixel length
-  
-  
-  
-  for(var x=0; x < num*sideLen; x = x+sideLen){
-    for(var y=0; y < num*sideLen; y = y+sideLen){
-      
-      fill(random(0, 400),random(0, 400), random(0, 400)); // random color RGB
-      quad(x + random (-10, 10),y + random (-10, 10), x+sideLen + random (-10, 10),y + random (-10, 10), x+sideLen + random (-10, 10),y+sideLen + random (-10, 10), x + random (-10, 10),y+sideLen + random (-10, 10));
+    background(255);
+
+    var windowPane = 10; // padding around box array
+    var num = 3; //number of boxes in array
+    var shift = random(-5, 5); //degree of shift in quad
+    var space = 50; //space between squares
+
+    translate(windowPane + space / 2, windowPane + space / 2); //(x,y) start point offset for padding
+
+    var sideLen = (windowWidth - 2 * windowPane) / num //length of individual side of a box
+
+    for (var x = 0; x < num * sideLen; x = x + sideLen) {
+        for (var y = 0; y < num * sideLen; y = y + sideLen) {
+
+            fill(random(300, 270), random(100,255), random(0, 255));
+
+            //a random shift is added to each vertex
+            // add length of individual side of a box (num) and 
+            //subtract space between squares (space) to keep spacing true
+
+            quad(random(-shift, shift) + x,
+                random(-shift, shift) + y,
+
+                random(-shift, shift) + x + sideLen - space,
+                random(-shift, shift) + y,
+
+                random(-shift, shift) + x + sideLen - space,
+                random(-shift, shift) + y + sideLen - space,
+
+                random(-shift, shift) + x,
+                random(-shift, shift) + y + sideLen - space);
+        }
     }
-  }
-  
-  noLoop();
 
 }
-  
